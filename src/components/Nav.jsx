4e352a3692c94fb27da9a2ee2f64a02746884c09
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-02-21 21:36:13 +0530

    Add Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="">Logo</div>
+      <div className="">Name</div>
+      <button className="button">Button</button>
+    </nav>
+  );
+};
+
+export default Nav;
