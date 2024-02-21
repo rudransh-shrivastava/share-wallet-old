b492e8a19fb3a79d6a7fe64cd531eb3fc92c2fa6
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-02-21 21:35:52 +0530

    Remove default logo and add sharewallet.jpg

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Nav from './components/Nav.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Nav />
+  </React.StrictMode>
+);
