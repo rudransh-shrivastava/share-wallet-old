68ecf65f29ded036552a1e3d26d87265898e8a93
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-02-29 19:04:04 +0530

    Fix Login Button

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -16,21 +16,12 @@ const Nav = () => {
           Share Wallet
         </div>
       </div>
-      {/* <a href="/login/google"> */}
-      {/* NOTE: Change the onClick Function  */}
-      <button
-        className="bg-white hover:bg-emerald-700 text-accentDark px-4 py-2 rounded-md"
-        onClick={openLoginPopup}
-      >
-        Login
-      </button>
-      {/* </a> */}
-      {/* TODO: Change to Logout If already Logged In */}
+      <a href="http://localhost:3001/auth/google">
+        <button className="bg-white hover:bg-emerald-700 text-accentDark px-4 py-2 rounded-md">
+          Login
+        </button>
+      </a>
     </nav>
   );
 };
-// TODO: Gopal: Create a popup for login, the popup should include only one input field, the email id, and a Login button, it should also include a Sign Up button next to the Login button and that should open a new popup with the signup form, then signup form is submitted with the /CreateUser API
-function openLoginPopup() {
-  console.log('Login Popup Opened');
-}
 export default Nav;
