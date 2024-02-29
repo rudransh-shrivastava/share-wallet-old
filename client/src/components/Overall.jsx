777250b26a3a182dfb779fbae6a8f5622f8abb52
Gopal <gopal.lohar.dev@gmail.com>
2024-03-01 00:02:35 +0530

    make fetch overall from server work

--- client/src/components/Overall.jsx
+++ client/src/components/Overall.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '../assets/data.json';
 import axios from 'axios';
 
 const Overall = () => {
+  function getUserTotal(user) {
+    axios
+      .get('http://localhost:3001/total', { params: { user } })
+      .then((res) => {
+        setOverall({ ...res.data, loaded: true });
+      });
+  }
+
+  const [overall, setOverall] = useState({ loaded: false });
   const { totalBalance, youOwe, youAreOwed } = data.overall;
+  // TODO: make it reload when we change other data
+  useEffect(() => {
+    getUserTotal('Gopal');
+  }, []);
 
   return (
     <div className="flex w-full justify-center gap-8">
       <div className="flex flex-col items-center p-4">
         <span>Total Balance</span>
-        <span className="text-2xl">+ &#8377;{totalBalance}</span>
+        <span className="text-2xl">
+          {overall.loaded ? `${Number(overall.total) < 0 ? '-' : ''}` : ''}
+          &#8377;
+          {`${overall.loaded ? Math.abs(Number(overall.total)) : 'Loading'}`}
+        </span>
       </div>
       <div className="flex flex-col items-center p-4">
         <span>You Owe</span>
-        <span className="text-2xl">&#8377;{youOwe}</span>
+        <span className="text-2xl">
+          &#8377;{overall.loaded ? overall.owes : 'Loading'}
+        </span>
       </div>
       <div className="flex flex-col items-center p-4">
         <span>You are Owed</span>
-        <span className="text-2xl">&#8377;{youAreOwed}</span>
+        <span className="text-2xl">
+          &#8377;{overall.loaded ? overall.owed : 'Loading'}
+        </span>
       </div>
     </div>
   );
 };
 
-// TODO: Gopal: Load this data everytime the page is loaded
-function getUserTotal(user) {
-  axios.get('http://localhost:3001/total', { params: { user } }).then((res) => {
-    console.log(res.data);
-  });
-}
-getUserTotal('Gopal');
 export default Overall;
