f6690df8f764ae927d3453c78dcac46e54be76be
Gopal <gopal.lohar.dev@gmail.com>
2024-03-07 04:41:21 +0530

    add colors and modificaitons with overall

--- client/src/components/Overall.jsx
+++ client/src/components/Overall.jsx
@@ -16,7 +16,7 @@ const Overall = () => {
   }, [showPopup]);
 
   return (
-    <div className="flex w-full justify-center sm:gap-4 md:gap-8 py-2">
+    <div className="flex flex-wrap w-full justify-center sm:gap-4 md:gap-8 py-2">
       {userTotalError ? 'Something went wrong' : ''}
       {userTotalLoading ? (
         <LoadingSpinner />
@@ -24,19 +24,29 @@ const Overall = () => {
         <>
           <div className="flex flex-col items-center p-2">
             <span className="text-center">Total Balance</span>
-            <span className="text-2xl">
-              {Number(userTotal.total) < 0 ? '-' : ''}
+            <span
+              className={`text-2xl ${
+                Number(userTotal.total) < 0
+                  ? 'text-red-700 dark:text-red-500'
+                  : 'text-green-700 dark:text-green-400'
+              }`}
+            >
+              {Number(userTotal.total) < 0 ? '-' : '+'}
               &#8377;
-              {Math.abs(Number(userTotal.total))}
+              {Math.abs(Number(userTotal.total?.toFixed(2)))}
             </span>
           </div>
           <div className="flex flex-col items-center p-2">
             <span>You Owe</span>
-            <span className="text-2xl">&#8377;{userTotal.owes}</span>
+            <span className="text-2xl">
+              &#8377;{userTotal.owes?.toFixed(2)}
+            </span>
           </div>
           <div className="flex flex-col items-center p-2">
             <span>You are Owed</span>
-            <span className="text-2xl">&#8377;{userTotal.owed}</span>
+            <span className="text-2xl">
+              &#8377;{userTotal.owed?.toFixed(2)}
+            </span>
           </div>
         </>
       )}
